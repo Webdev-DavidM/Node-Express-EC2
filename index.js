117c47a1bed5f7c1cb9607d59b61639476d08424
David Mulholland <david@futr.ai>
2023-11-21 10:46:38 +0000

    user can now post a place to this end point

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import OpenAI from "openai";
+import bodyParser from "body-parser";
 
 const openai = new OpenAI({
   apiKey: `${process.env.OPENAI_KEY}`,
@@ -9,21 +10,27 @@ const openai = new OpenAI({
 
 const app = express();
 
-export const getStreamingCompletion = async ({ userPrompt }) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+export const getStreamingCompletion = async ({ prompt }) => {
   return openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: userPrompt }],
+    messages: [{ role: "user", content: prompt }],
     stream: true,
   });
 };
 
-app.get("/", cors(), async (req, res) => {
-  const data = req.body;
-  const stream = await getStreamingCompletion({ userPrompt: "London" });
+app.post("/", cors(), async (req, res) => {
+  const place = req.body.place;
+  console.log(place);
+  const stream = await getStreamingCompletion({
+    prompt: `Please tell me the best five places to visit in ${place}`,
+  });
   for await (const part of stream) {
+    console.log(part.choices[0]?.delta.content || "");
     res.write(part.choices[0]?.delta.content || "");
   }
-
   res.end();
 });
 
