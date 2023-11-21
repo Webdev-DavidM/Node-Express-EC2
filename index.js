6eedc26ce9b9d9794e0452f504ae48460cae2e75
David Mulholland <david@futr.ai>
2023-11-21 10:26:50 +0000

    code clean up

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-//include express module or package
-
 import express from "express";
 import cors from "cors";
-
+import "dotenv/config";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: `${process.env.OPENAI_API_KEY}`,
+  apiKey: `${process.env.OPENAI_KEY}`,
 });
 
 const app = express();
@@ -20,38 +18,13 @@ export const getStreamingCompletion = async ({ userPrompt }) => {
 };
 
 app.get("/", cors(), async (req, res) => {
-  //   const completion = await openai.chat.completions.create({
-  //     model: "gpt-3.5-turbo",
-  //     messages: [
-  //       {
-  //         role: "user",
-  //         content: `Please tell me the best five places to visit in London`,
-  //       },
-  //     ],
-  //     stream: true,
-  //   });
-
   const data = req.body;
-  //   let starttime = Date.now();
   const stream = await getStreamingCompletion({ userPrompt: "London" });
   for await (const part of stream) {
-    // here express will stream the response
-    console.log(part.choices[0]?.delta.content || "");
     res.write(part.choices[0]?.delta.content || "");
   }
-  // here express sends the closing/done/end signal for the stream consumer
+
   res.end();
-
-  //   for await (const chunk of completion) {
-  //     if (chunk?.choices[0]?.delta?.content) {
-  //       res.write(chunk?.choices[0]?.delta?.content || "");
-  //     }
-  //     console.log(chunk.choices[0].delta.content);
-  //     // if (chunk.choices[0].finish_reason === "stop") return res.end();
-
-  //     // res.write(chunk.choices[0].delta.content);
-  //   }
-  //   res.end();
 });
 
 app.listen(4000, () => console.log("Listening on port 4000!"));
